322b756e9ba5f071e7d28459298eceb2ad07e9f6
ks603 <kevin.shaughnessy18@gmail.com>
2020-06-01 17:42:52 -0400

    Created firebase backend and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+  apiKey: "AIzaSyAAKngYyPWqQWgpvTt1lgY3luSA9iv_SQQ",
+  authDomain: "thread-react.firebaseapp.com",
+  databaseURL: "https://thread-react.firebaseio.com",
+  projectId: "thread-react",
+  storageBucket: "thread-react.appspot.com",
+  messagingSenderId: "256461229134",
+  appId: "1:256461229134:web:a02e8b5f262af12c84c71f",
+  measurementId: "G-6RCP0B6GXG"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase
