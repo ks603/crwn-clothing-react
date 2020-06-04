e47c34591290063260858fb9baa82cfac85e8671
ks603 <kevin.shaughnessy18@gmail.com>
2020-06-04 19:14:06 -0400

    Created cart dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 
 import { auth } from '../../firebase/firebase.utils.js'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
       <CartIcon />
 
     </div>
+    <CartDropdown />
 
   </div>
 )
