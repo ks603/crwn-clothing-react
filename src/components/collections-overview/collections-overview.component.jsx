732b18335382d88ca9d455878e7ca1026b10e558
ks603 <kevin.shaughnessy18@gmail.com>
2020-06-07 22:03:04 -0400

    Shop overview is now its own component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import CollectionPreview from '../preview-collection/collection-preview.component'
+
+import './collections-overview.styles.scss'
+import { selectCollections } from '../../redux/shop/shop.selector'
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="colelctions-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(CollectionsOverview)
+
