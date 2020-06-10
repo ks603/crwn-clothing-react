a23db0d954e1cf521606e5620fa02c8de1b741e3
ks603 <kevin.shaughnessy18@gmail.com>
2020-06-10 17:07:43 -0400

    Modified for mobile styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import './App.css';
+
 
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component'
+import { GlobalStyle } from './globabl.styles'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector'
@@ -43,6 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
